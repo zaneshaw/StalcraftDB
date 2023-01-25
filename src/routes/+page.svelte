52f876c3b15509ad7b61d57af59a40ded80da46f
Zane Shaw <admin@triangle100.com>
2023-01-26 02:30:21 +1100

    Use border for anchor underline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,17 @@
 	import image from "$lib/images/favicon.png";
 </script>
 
-<div class="flex h-screen flex-col items-center justify-center gap-8 text-center">
+<div class="mx-auto flex h-screen w-fit flex-col items-center justify-center gap-8 text-center">
 	<img src={image} alt="" class="w-60" />
 	<div class="flex flex-col gap-4">
 		<h1>Your spot for Stalcraft</h1>
 		<h1 class="text-sky-400">Weapons</h1>
 	</div>
-	<h3 class="text-gray-400">
+	<h3 class="w-3/5 text-gray-400">
 		Find all your favourite items on <em class="glow text-red-100">StalcraftDB</em> via
-		<a href="https://eapi.stalcraft.net" target="_blank" rel="noreferrer">
-			EXBO's official Stalcraft API
-		</a>
+		<a href="https://eapi.stalcraft.net" target="_blank" rel="noreferrer"
+			>EXBO's official Stalcraft API</a
+		>.
 	</h3>
 	<div class="flex gap-4">
 		<button class="btn">Explore</button>
