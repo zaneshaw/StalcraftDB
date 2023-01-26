215683e46b13a2e4d0ef45622ebba95680fe555c
Zane Shaw <admin@triangle100.com>
2023-01-26 23:03:56 +1100

    Add hero image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
-	import image from "$lib/images/favicon.png";
+	import image from "$lib/assets/favicon.png";
+	import hero from "$lib/assets/hero_notext.jpg";
 </script>
 
-<div class="mx-auto flex h-screen w-fit flex-col items-center justify-center gap-8 text-center">
-	<img src={image} alt="" class="w-60" />
-	<div class="flex flex-col gap-4">
-		<h1>Your spot for Stalcraft</h1>
-		<h1 class="text-sky-400">Weapons</h1>
+<div class="relative h-screen overflow-hidden">
+	<img src={hero} alt="" class="absolute top-0 -z-10 opacity-20" />
+	<div class="mx-auto flex h-full w-fit flex-col items-center justify-center gap-8 text-center">
+		<img src={image} alt="" class="w-60" />
+		<div class="flex flex-col gap-4">
+			<h1>Your spot for Stalcraft</h1>
+			<h1 class="text-sky-400">Weapons</h1>
+		</div>
+		<h3 class="w-3/5 text-gray-400">
+			Find all your favourite items on <em class="glow text-gray-100">StalcraftDB</em> via
+			<a href="https://eapi.stalcraft.net" target="_blank" rel="noreferrer"
+				>EXBO's official Stalcraft API</a
+			>.
+		</h3>
+		<div class="flex gap-4">
+			<button class="btn">Explore</button>
+			<button class="btn btn-outline">GitHub</button>
+		</div>
 	</div>
-	<h3 class="w-3/5 text-gray-400">
-		Find all your favourite items on <em class="glow text-red-100">StalcraftDB</em> via
-		<a href="https://eapi.stalcraft.net" target="_blank" rel="noreferrer"
-			>EXBO's official Stalcraft API</a
-		>.
-	</h3>
-	<div class="flex gap-4">
-		<button class="btn">Explore</button>
-		<button class="btn btn-outline">GitHub</button>
-	</div>
+</div>
+<div class="flex flex-col items-center bg-gray-900 py-12">
+	<h1>test</h1>
 </div>
